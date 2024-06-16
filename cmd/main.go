bc4455eed119cac47afe39c966bb57c01d168c05
= <153317476+scatternoodle@users.noreply.github.com>
2024-06-16 13:15:03 +0100

    implement basic REPL

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
-	"github.com/scatternoodle/wflang/lang/lexer"
-	"github.com/scatternoodle/wflang/lang/parser"
+	"github.com/scatternoodle/wflang/lang/repl"
 )
 
 func main() {
-	s := `"hey"`
-	l := lexer.New(s)
-	p := parser.New(l)
-	ast := p.Parse()
-
-	fmt.Print("\n\nAST:\n\n")
-	if ast != nil {
-		fmt.Println(ast.String())
+	if err := repl.Run(); err != nil {
+		log.Fatal(err)
 	}
-
-	fmt.Print("\n\nErrors:\n\n")
-	errs := p.Errors()
-	if len(errs) > 0 {
-		for _, err := range errs {
-			fmt.Println(err.Error())
-		}
-	}
-
-	fmt.Print("\n\nTrace:\n\n")
-	fmt.Println(p.Trace())
 }
---
+++ lang/repl/repl.go
@@ -0,0 +1,45 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/scatternoodle/wflang/lang/lexer"
+	"github.com/scatternoodle/wflang/lang/parser"
+)
+
+func Run() error {
+	fmt.Println("Welcome to WFLANG.\nPlease enter formula, or '.q' to quit.")
+
+	scn := bufio.NewScanner(os.Stdin)
+	for {
+		if !scn.Scan() {
+			if err := scn.Err(); err != nil {
+				return err
+			}
+			return nil
+		}
+
+		txt := scn.Text()
+		if strings.ToLower(txt) == ".q" {
+			fmt.Println("Buh-bye.")
+			return nil
+		}
+
+		prs := parser.New(lexer.New(txt))
+		AST := prs.Parse()
+
+		errs := prs.Errors()
+		if len(errs) > 0 {
+			fmt.Println("Parser encountered the following errors:")
+			for i, err := range errs {
+				fmt.Printf("\t[%d]: %s\n", i, err.Error())
+			}
+			return nil
+		}
+
+		fmt.Println(AST.String())
+	}
+}
