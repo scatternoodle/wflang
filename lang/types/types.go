46417399ff597c272804d7b850b6f10b303e5b1b
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-07-14 00:57:00 +0100

    more type definitions

--- lang/types/types.go
+++ lang/types/types.go
@@ -3,16 +3,20 @@ package types
 type BaseType string
 
 const (
-	NUMBER   BaseType = "number"
-	STRING   BaseType = "string"
-	TIME     BaseType = "time"
-	DTTM     BaseType = "datetime"
-	DTTMRNG  BaseType = "datetime range"
-	DATE     BaseType = "date"
-	BOOL     BaseType = "boolean"
-	SCHEDREC BaseType = "schedule record"
-	TIMEREC  BaseType = "time record"
-	EMPATTR  BaseType = "employee attribute"
-	LDREC    BaseType = "LD record"
-	TORDTL   BaseType = "TOR detail record"
+	NUMBER    BaseType = "number"
+	STRING    BaseType = "string"
+	TIME      BaseType = "time"
+	DTTM      BaseType = "datetime"
+	DTTMRNG   BaseType = "datetime range"
+	DATE      BaseType = "date"
+	DATERNG   BaseType = "date range"
+	BOOL      BaseType = "boolean"
+	SCHEDREC  BaseType = "schedule record"
+	TIMEREC   BaseType = "time record"
+	EMPATTR   BaseType = "employee attribute"
+	LDREC     BaseType = "LD record"
+	TORDTL    BaseType = "TOR detail record"
+	OBJECT    BaseType = "object" // misc type for things like SQL results
+	POLICY_ID BaseType = "policy ID"
+	PERIOD    BaseType = "period"
 )
