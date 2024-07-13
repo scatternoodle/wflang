749c6ed0f0a271c2fb5bf5f44f5786c9da1cc34f
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-07-13 22:26:05 +0100

    disable ad hoc testing code in repl main

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"log/slog"
 
-	"github.com/scatternoodle/wflang/lang/lexer"
-	"github.com/scatternoodle/wflang/lang/parser"
+	"github.com/scatternoodle/wflang/lang/repl"
 )
 
 func main() {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	// log.Fatal(repl.Run())
+	log.Fatal(repl.Run())
 
-	input := `/* 1 */
-/* 2.1
-2.2 */`
+	// 	input := `/* 1 */
+	// /* 2.1
+	// 2.2 */`
 
-	prs := parser.New(lexer.New(input))
+	// 	prs := parser.New(lexer.New(input))
 
-	fmt.Println("errors:")
-	for _, e := range prs.Errors() {
-		fmt.Printf("%+v\n", e)
-	}
-	fmt.Print("\n\n")
+	// 	fmt.Println("errors:")
+	// 	for _, e := range prs.Errors() {
+	// 		fmt.Printf("%+v\n", e)
+	// 	}
+	// 	fmt.Print("\n\n")
 
-	fmt.Println("tokens:")
-	for _, t := range prs.Tokens() {
-		fmt.Printf("%+v\n", t)
-	}
+	// fmt.Println("tokens:")
+	//
+	//	for _, t := range prs.Tokens() {
+	//		fmt.Printf("%+v\n", t)
+	//	}
 }
