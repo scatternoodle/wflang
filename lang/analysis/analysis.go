78613cc6bdad7cc6f9c7f8d3c313fe3ea5095fe8
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-07-15 19:53:02 +0100

    wip - pause evaluator

--- lang/analysis/analysis.go
+++ lang/analysis/analysis.go
@@ -4,9 +4,16 @@ import (
 	"github.com/scatternoodle/wflang/lang/ast"
 	"github.com/scatternoodle/wflang/lang/builtins"
 	"github.com/scatternoodle/wflang/lang/lexer"
+	"github.com/scatternoodle/wflang/lang/types"
 )
 
-func Evaluate(program *ast.AST) {}
+type object struct {
+	baseType    types.BaseType
+	environment map[string]object
+}
+
+func Evaluate(ast.Node) {
+}
 
 // isReserved returns true if string is a reserved language keyword.
 func IsReserved(s string) bool {
