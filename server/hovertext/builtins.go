c9a42cc7fb28f13fdc20af51212b7d26370b4cca
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-07-21 09:30:10 +0100

    simplify hovertext format a little

--- server/hovertext/builtins.go
+++ server/hovertext/builtins.go
@@ -10,7 +10,7 @@ const (
 		"min(args: number...)\n" +
 		CodeBlockEnd +
 		"### Min\n" +
-		"Min returns the smallest of its arguments. It takes a list of arguments as long as you like, which can be any" +
+		"Returns the smallest of its arguments. It takes a list of arguments as long as you like, which can be any" +
 		" expression that evaluates to a number.\n\n" +
 		"@param `args: number...` - list of numbers to compare\n\n"
 
@@ -18,7 +18,7 @@ const (
 		"max(args: number...)\n" +
 		CodeBlockEnd +
 		"### Max\n" +
-		"Max returns the largest of its arguments. It takes a list of arguments as long as you like, which can be any" +
+		"Returns the largest of its arguments. It takes a list of arguments as long as you like, which can be any" +
 		" expression that evaluates to a number.\n\n" +
 		"@param `args: number...` - list of numbers to compare\n\n"
 
@@ -26,7 +26,7 @@ const (
 		"contains(string1: string, string2: string)\n" +
 		CodeBlockEnd +
 		"### Contains\n" +
-		"Contains returns true if `string2` is a substring of `string1`.\n\n" +
+		"Returns true if `string2` is a substring of `string1`.\n\n" +
 		"@param `string1: string` - the string to search in\n\n" +
 		"@param `string2: string` - the string to search for\n\n"
 
@@ -36,7 +36,7 @@ const (
 		"  [, where condition: boolean] )\n" +
 		CodeBlockEnd +
 		"### Sum\n" +
-		"Sum calculates the sum of a numeric expression repeatedly over `range`.\n\n" +
+		"Calculates the sum of a numeric expression repeatedly over `range`.\n\n" +
 		"@param `interval: day|week|period` - the time period to group by\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to sum\n\n" +
 		"@param `aliasName: string` - alias for the interval currently being evaluated\n\n" +
@@ -48,7 +48,7 @@ const (
 		"     , where condition: boolean )\n" +
 		CodeBlockEnd +
 		"### Count\n" +
-		"Count calculates a total count of qualifying intervals across `range`.\n\n" +
+		"Calculates a total count of qualifying intervals across `range`.\n\n" +
 		"@param `interval: day|week|period` - the time period to group by\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to count\n\n" +
 		"@param `aliasName: string` - alias for the interval currently being evaluated\n\n" +
@@ -73,7 +73,7 @@ const (
 		"      [, where whereExpression?: bool] )\n" +
 		CodeBlockEnd +
 		"### SumTime\n" +
-		"SumTime calculates the sum of a numeric expression over a range of time records.\n\n" +
+		"Calculates the sum of a numeric expression over a range of time records.\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to sum\n\n" +
 		"@param `aliasName?: string` - alias for the slice currently being evaluated\n\n" +
 		"@param `expression: number` - numeric expression - this is what will be summed for each qualifying slice\n\n" +
@@ -84,7 +84,7 @@ const (
 		"        [, where whereExpression?: bool] )\n" +
 		CodeBlockEnd +
 		"### CountTime\n" +
-		"CountTime calculates the count of time records in a range.\n\n" +
+		"Calculates the count of time records in a range.\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to count\n\n" +
 		"@param `aliasName?: string` - alias for the slice currently being evaluated\n\n" +
 		"@param `whereExpression?: boolean` - if used, only slices where this expression returns true will be counted\n\n"
@@ -95,7 +95,7 @@ const (
 		"             , order by ordering: string|number|date|dateTime )\n" +
 		CodeBlockEnd +
 		"### FindFirstTime\n" +
-		"FindFirstTime returns the first time record that meets `condition`, ordered by `ordering`.\n\n" +
+		"Returns the first time record that meets `condition`, ordered by `ordering`.\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to search\n\n" +
 		"@param `aliasName?: string` - alias for the slice currently being evaluated\n\n" +
 		"@param `condition: boolean` - the condition to evaluate\n\n" +
@@ -107,7 +107,7 @@ const (
 		"          [, where whereExpression?: bool] )\n" +
 		CodeBlockEnd +
 		"### SumSchedule\n" +
-		"SumSchedule calculates the sum of a numeric expression over a range of schedule records.\n\n" +
+		"Calculates the sum of a numeric expression over a range of schedule records.\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to sum\n\n" +
 		"@param `aliasName?: string` - alias for the slice currently being evaluated\n\n" +
 		"@param `expression: number` - numeric expression - this is what will be summed for each qualifying slice\n\n" +
@@ -118,7 +118,7 @@ const (
 		"            [, where whereExpression?: bool] )\n" +
 		CodeBlockEnd +
 		"### CountSchedule\n" +
-		"CountSchedule calculates the count of schedule records in a range.\n\n" +
+		"Calculates the count of schedule records in a range.\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to count\n\n" +
 		"@param `aliasName?: string` - alias for the slice currently being evaluated\n\n" +
 		"@param `whereExpression?: boolean` - if used, only slices where this expression returns true will be counted\n\n"
@@ -129,7 +129,7 @@ const (
 		"                 , order by ordering: string|number|date|dateTime )\n" +
 		CodeBlockEnd +
 		"### FindFirstSchedule\n" +
-		"FindFirstSchedule returns the first schedule record that meets `condition`, ordered by `ordering`.\n\n" +
+		"Returns the first schedule record that meets `condition`, ordered by `ordering`.\n\n" +
 		"@param `range: day|week|period|dateRange` - the time period over which to search\n\n" +
 		"@param `aliasName?: string` - alias for the slice currently being evaluated\n\n" +
 		"@param `condition: boolean` - the condition to evaluate\n\n" +
