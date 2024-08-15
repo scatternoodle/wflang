702f8e5b3b5028f3f5b7a09cdca9f2875c331629
scatternoodle <153317476+scatternoodle@users.noreply.github.com>
2024-08-15 10:33:35 +0100

    remove temp debug code

--- server/document_symbols.go
+++ server/document_symbols.go
@@ -1,31 +1,16 @@
 package server
 
 import (
-	"log/slog"
-
 	"github.com/scatternoodle/wflang/lsp"
 )
 
 func (srv *Server) documentSymbols() []lsp.DocumentSymbol {
-
-	// @TEMPLOG
-	slog.Debug("preparing symbols for variables")
-	prs := srv.parser
-	slog.Debug("Parser state",
-		"statements", prs.Statements(),
-		"errors", prs.Errors(),
-		"vars", prs.Vars(),
-	)
-
 	symbols := []lsp.DocumentSymbol{}
 
 	for _, v := range srv.parser.Vars() {
 		if v.Statement == nil {
 			continue
 		}
-
-		// @TEMPLOG
-		slog.Debug("variable", "name", v.Name, "statement", v.Statement)
 
 		varStart, varEnd := v.Statement.Pos()
 		symbolRange := lsp.Range{
