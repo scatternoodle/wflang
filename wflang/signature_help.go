59fea28b6a5c63d06c8c3b34a336c0582327d435
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2024-09-13 19:54:52 +0100

    wip

--- wflang/signature_help.go
+++ wflang/signature_help.go
@@ -3,6 +3,14 @@ package wflang
 import "github.com/scatternoodle/wflang/internal/lsp"
 
 func SignatureHelp(pos lsp.Position) (info lsp.SignatureInfo, activeParam int, err error) {
+	// CURRENT
+	// get all nodes within range
+	// find callExpression in those nodes
+	// look up function object
+	// get params
+	// convert them to protocol info
+	// determine active param
+	// return sig info
 
 	return lsp.SignatureInfo{}, 0, nil
 }
